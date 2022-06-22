f22619818a4fe2925041a4fe251da75b69adab30
Christopher Hardy <chrishardy100@gmail.com>
2022-06-22 13:32:07 -0400

    Create biggerDigits.cpp

---
+++ biggerDigits.cpp
@@ -0,0 +1,35 @@
+//this 
+#include <iostream>
+#include <cmath>
+using namespace std;
+int biggerDigits(int a, int b) {
+    int choose = 0, big = 0, temp = 0;
+
+    for (int i = 10; i < 100000; i *= 10) {
+        if ((a % i) > (b % i)) {
+            choose = (a % i) - big;
+        }
+        if ((b % i) > (a % i)) {
+            choose = (b % i) - big;
+
+        }
+        big = big + choose;
+    }
+    //if(a > b)big = big + ( )
+    return big;
+}
+int main() {
+
+    //INPUT
+    cout << "Enter 2 numbers" << endl;
+    int x, y;
+    cin >> x;
+    cin >> y;
+    
+    //OUTPUT
+    cout << biggerDigits(x, y); //print 2445
+    //cout << 0 + ((x  % 100) -10);
+    return 0;
+}
+
+
